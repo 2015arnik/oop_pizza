2526b6d917140bcd7c97be1a4f2dc9a295aa6eba
2015arnik <2015arnik@mail.ru>
2026-02-22 23:35:02 +0700

    added Base class

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-abstract class Enity {
+abstract class Entity {
     protected String name;
 
-    public Enity (String name) {
+    public Entity (String name) {
         this.name = name;
     }
 
@@ -14,22 +14,75 @@ abstract class Enity {
     }
 }
 
-class Ingredient extends Enity {
+class Ingredient extends Entity {
     private double price;
 
     public Ingredient (String name, double price) {
         super(name);
-        this.price = price
+        this.price = price;
     }
 
     public double getPrice() {
         return price;
     }
     
-    public void setPrice(double cost) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
+}
+
+abstract class Base extends Entity {
+    protected double price;
+    protected static final double  CLASSIC_BASE_PRICE = 100.0;
+
+    public Base (String name, double price) {
+        super(name);
+        this.price = price;
+    }
+
+    public double getPrice () {
+        return price;
+    } 
+
+    public void setPrice (double price) {
+        this.price = price;
+    }
+}
+
+class ClassicBase extends Base {
+    public ClassicBase () {
+        super("Классическая", CLASSIC_BASE_PRICE);
+    }
+
+    @Override
+    public void setPrice(double price) {
+        this.price = CLASSIC_BASE_PRICE;
+    }
+}
+
+class NotClassicBase extends Base {
+        private static double checkedPrice(double price) {
+        double maxPrice = CLASSIC_BASE_PRICE * 1.2;
+        if (price > maxPrice) {
+            throw new IllegalArgumentException(
+                String.format("Стоимость не должна превышать %.2f руб. (20%% от классической)", maxPrice)
+            );
+        }
+        return price;
+    }
+
+    public NotClassicBase(String name, double price) {
+        super(name, checkedPrice(price));
+    }
+
+    @Override
+    public void setPrice (double price) {
+        double maxPrice = CLASSIC_BASE_PRICE * 1.2;
+        if (price > maxPrice) throw new IllegalArgumentException(
+                String.format("Стоимость не должна превышать %.2f руб. (20%% от классической)", maxPrice));
+        else this.price = price;
+    }
 }
 
 
